2975d7a8ea859fe770251d307c633cbcc7c1283f
Vanessa <vanessalins356@gmail.com>
2025-02-17 11:35:15 -0300

    tres

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello(@RequestParam String name) {
+    public String hello(@RequestParam(defaultValue = "World") String name) {
         return "Hello " + name;
     }
 }
